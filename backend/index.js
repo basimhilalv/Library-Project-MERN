e83dd1ace74b86bc41c427ff94b5217c58dee645
Basim Hilal V <basimhilalofficial@gmail.com>
2023-11-22 11:47:53 +0530

    frontend is configured with react

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import { MONGODB_URL, PORT } from "./config.js";
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
 import booksRouter from "./routes/booksRouter.js";
+import cors from 'cors';
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+// app.use(cors({
+//     origin:'http://localhost3000',
+//     method:['GET','POST','PUT','DELETE'],
+//     allowedHeaders:['Content-Type']
+// }));
 
 app.get('/', (request, response) => {
     console.log(request)
